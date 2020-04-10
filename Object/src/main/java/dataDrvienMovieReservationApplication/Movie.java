da701c0869ee8b61209a46e30d5c78a4023c043d
PandaHun <gnsgnsek@gmail.com>
2020-04-10 23:56:43 +0900

    feature: DataDrivenDevelope MovieReservation Application
    데이터 중심의 영화 예메 시스템 구현

---
+++ Object/src/main/java/dataDrvienMovieReservationApplication/Movie.java
@@ -0,0 +1,74 @@
+package dataDrvienMovieReservationApplication;
+
+import movieReservationApplication.Money;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Duration getRunningTime() {
+        return runningTime;
+    }
+
+    public void setRunningTime(Duration runningTime) {
+        this.runningTime = runningTime;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDisCountConditions() {
+        return discountConditions;
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> disCountConditions) {
+        this.discountConditions = disCountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+}
