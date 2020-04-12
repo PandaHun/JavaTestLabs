e03b61ce88ff4a6799287eaf5f3bb34afb3d7484
PandaHun <gnsgnsek@gmail.com>
2020-04-12 18:52:32 +0900

    jpa database setting
    하지만 test fail

---
+++ Jpa/src/test/java/com/jpa/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.jpa.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUserName("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member :  " + (findMember == member));    //영속성 컨텍스트에 의해 원래 있던거라 조회한것이랑 같다고 인식
+    }
+}
