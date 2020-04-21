19ce3ddc86cf528385ab93fe119790fdd7690f6c
PandaHun <gnsgnsek@gmail.com>
2020-04-22 00:36:32 +0900

    refactor: encapsulate data

--- Object/src/main/java/dataDrvienMovieReservationApplication/ReservationAgency.java
+++ Object/src/main/java/dataDrvienMovieReservationApplication/ReservationAgency.java
@@ -5,41 +5,7 @@ import movieReservationApplication.Money;
 public class ReservationAgency {
 
     public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
-        Movie movie = screening.getMovie();
-
-        boolean discountAble = false;
-        for (DiscountCondition condition : movie.getDisCountConditions()) {
-            if (condition.getType() == DiscountConditionType.PERIOD) {
-                discountAble = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
-                        condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
-                        condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
-            } else {
-                discountAble = condition.getSequence() == screening.getSequence();
-            }
-            if(discountAble) {
-                break;
-            }
-        }
-        Money fee;
-        if(discountAble) {
-            Money discountAmount = Money.ZERO;
-            switch (movie.getMovieType()) {
-                case AMOUNT_DISCOUNT:
-                    discountAmount = movie.getDiscountAmount();
-                    break;
-                case PERCENT_DISCOUNT:
-                    discountAmount = movie.getFee().times(movie.getDiscountPercent());
-                    break;
-                case NONE_DISCOUNT:
-                    discountAmount = Money.ZERO;
-                    break;
-            }
-            fee = movie.getFee().minus(discountAmount).times(audienceCount);
-        } else {
-            fee = movie.getFee();
-        }
+        Money fee = screening.calculateFee(audienceCount);
         return new Reservation(customer, screening, fee, audienceCount);
-
     }
-
 }
--- Object/src/main/java/dataDrvienMovieReservationApplication/Screening.java
+++ Object/src/main/java/dataDrvienMovieReservationApplication/Screening.java
@@ -1,5 +1,7 @@
 package dataDrvienMovieReservationApplication;
 
+import movieReservationApplication.Money;
+
 import java.time.LocalDateTime;
 
 public class Screening {
@@ -7,6 +9,12 @@ public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
 
     public Movie getMovie() {
         return movie;
@@ -30,5 +38,23 @@ public class Screening {
 
     public void setWhenScreened(LocalDateTime whenScreened) {
         this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
     }
 }
