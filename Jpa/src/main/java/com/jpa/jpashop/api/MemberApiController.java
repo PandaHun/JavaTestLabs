eb53ae32c11f24af0606d13a80641a1a6e1e2a40
PandaHun <gnsgnsek@gmail.com>
2020-05-28 21:05:51 +0900

    feat: create Member update api

--- Jpa/src/main/java/com/jpa/jpashop/api/MemberApiController.java
+++ Jpa/src/main/java/com/jpa/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package com.jpa.jpashop.api;
 
 import com.jpa.jpashop.domain.Member;
 import com.jpa.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -28,6 +27,34 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody UpdateMemberRequest request) {
+        memberService.update(id , request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(id, findMember.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
     @Data
     static class CreateMemberResponse {
         private Long id;
@@ -35,14 +62,6 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
-    }
-
-    @PostMapping("/api/v2/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid CreateMemberRequest request) {
-        Member member = new Member();
-        member.setName(request.getName());
-        Long id = memberService.join(member);
-        return new CreateMemberResponse(id);
     }
 
     @Data
