e2855f58d7c1c4eacb42a1e420c9e3091d1768fb
PandaHun <gnsgnsek@gmail.com>
2020-05-28 21:19:01 +0900

    feat: create Member read api

--- Jpa/src/main/java/com/jpa/jpashop/api/MemberApiController.java
+++ Jpa/src/main/java/com/jpa/jpashop/api/MemberApiController.java
@@ -8,12 +8,40 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> members = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(members);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     /*
     Version  1
@@ -38,7 +66,7 @@ public class MemberApiController {
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
                                                @RequestBody UpdateMemberRequest request) {
-        memberService.update(id , request.getName());
+        memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(id, findMember.getName());
     }
