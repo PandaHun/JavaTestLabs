e92ce361d509e98d101202eb2697e07e24c6cbf1
PandaHun <gnsgnsek@gmail.com>
2021-04-18 22:23:46 +0900

    feat: Init Locktest PJT

---
+++ locktest/src/main/java/org/panda/locktest/LocktestApplication.java
@@ -0,0 +1,13 @@
+package org.panda.locktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocktestApplication.class, args);
+    }
+
+}
